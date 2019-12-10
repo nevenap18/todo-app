ec67e638716de1434e81141341e7e664da7493e3
Nevena Pavlovic <nevena.pavlovic@ugbb.net>
2019-12-10 14:48:03 +0100

    mostly done

--- script.js
+++ script.js
@@ -1,177 +1,137 @@
+const addButton = document.getElementById('add');
+const input = document.getElementById('taskInput');
+const ul = document.getElementById('list'); // koristiti vec napravbljen selekor
 
-window.addEventListener('load', initialization);
+let array = JSON.parse(localStorage.getItem('todo'));
 
-function initialization(){
-
-    const addButton = document.getElementById("add");
-    addButton.addEventListener( "click" , newItem); 
-    
-    const input = document.getElementById("taskInput");
-    input.addEventListener("keypress", function(event) {
-    
-        let key = event.keyCode;
-    
-        if (key === 13) {
-            newItem();
-        }
-    });
-    
-    const ul = document.getElementById('list');
-    
-    ul.addEventListener("click", function(event){
-        let target = event.target;
-    
-        if (target.type == 'checkbox') {
-            toggleDone(target);
-            return;
-        }
-    
-        if (target.tagName === 'I') {
-            
-            deleteItem(target);                    //DELETE f-ja za niz/local storage
-            return;
-        }
-    }); 
+if (array == undefined) {
+    array = [];
 }
 
-function toggleDone(task){                              
-        task.classList.toggle("checked");
-        task.nextSibling.classList.toggle("crossed");
-        updateList(task);
-    }
+array.forEach(item => {
+    addToList(item);
+});
 
-function updateList(task){                                           
-    
-    let id = getId(task);
+function toggleDone(task) {
+    task.classList.toggle('checked');
+    task.nextSibling.classList.toggle('crossed');
+    updateList(task);
+}
 
-    let arrayItem = array.find(item => item.id == id);
+function updateList(task) {
+    let arrayItem = array.find(item => item.id == task.id);
     arrayItem.done = !arrayItem.done ? true : false;
-
-    //console.table(array);                                             // ++++ UPDATE LOCALSTORAGE
+    localStorage.setItem('todo', JSON.stringify(array));
 }
 
-function getTask(){                                                   //REQUIRED PRE ADD
-    const task = document.getElementById("taskInput").value;
-    return task;
-
+function deleteItem(target) {
+    let id = target.dataset.id;
+    saveDelete(id);
+    target.parentNode.remove();
 }
 
-function Item (id, task, done){
+function saveDelete(id) {
+    for (let i = 0; i <= array.length - 1; i++) {
+        if (array[i].id == id) {
+            array.splice(i, 1);
+        }
+    }
+    localStorage.setItem('todo', JSON.stringify(array));
+}
+
+function Item(id, task, done) {
     this.id = id;
     this.task = task;
     this.done = done;
-} 
-
-var array = [];
-
-function newItem () {
-
-    let id = 0;
-
-if (array.length != 0){
-    id = array[array.length-1].id + 1;
-};
-
-    const task = getTask();
-
-    if(task == ""){                                  //REQUIRED PRE ADD
-        alert("Please enter a task");
-        return;
-    } 
-
-    let item = new Item(id, task, false);
-
-    addToList(item);  
-    saveToStorage(item); // treba f-ja za prevodjenje u JSON koja se poziva u f-ji saveToStorage
-    const input = document.getElementById("taskInput");
-    input.value= "";
 }
 
-function newRow(item){
+function newItem() {
+    const task = document.getElementById('taskInput').value;
+    let id = 0;
     
-
-    let li = document.createElement("li");
-
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-
-    let idnumber = "" + item.id;
-    if (!item.done){
-        checkbox.classList.add("checkbox");
-        checkbox.setAttribute("id", idnumber);
-    } else {
-        checkbox.classList.add ("checkbox", "checked");
+    if (array.length != 0) {
+        id = array[array.length - 1].id + 1;
     };
 
-    let label = document.createElement("label");
-    label.setAttribute("class", "task");
-    label.setAttribute("for", idnumber)
+    if (task === '') {
+        alert("Please enter a task");
+        return;
+    }
+
+    const item = new Item(id, task, false);
+
+    array.push(item);
+    addToList(item);
+    saveToStorage();
+    input.value = '';
+}
+
+function newRow(item) {
+    const idnumber = item.id;
+
+    let li = document.createElement('li');
+
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.classList.add('checkbox');
+    checkbox.setAttribute('id', idnumber);
+
+    let label = document.createElement('label');
+    label.classList.add('task');
+    label.setAttribute('for', idnumber)
+
     let task = item.task;
     label.innerHTML = task;
 
-    let delButton = document.createElement("i");
-    delButton.innerHTML = "delete";
-    delButton.classList.add("material-icons", "icon-style");
+    if (item.done) {
+        checkbox.classList.add('checked');
+        label.classList.add('crossed')
+    };
+
+    let delButton = document.createElement('i');
+    delButton.innerHTML = 'delete';
+    delButton.classList.add('material-icons', 'delete-button');
+    delButton.setAttribute('data-id', idnumber);
 
     li.appendChild(checkbox);
     li.appendChild(label);
     li.appendChild(delButton);
-    
+
     return li;
-    
 }
 
-function addToList(item){
-    const ul = document.getElementById("list");
+function addToList(item) {
     let li = newRow(item);
     ul.appendChild(li);
     li.scrollIntoView();
-    array.push(item);
-
-
 }
 
-//CHECK:
+function saveToStorage() {
+    let key = 'todo';
+    let storageItem = JSON.stringify(array);
+    localStorage.setItem(key, storageItem);
+}
 
-function deleteItem (target){
+addButton.addEventListener('click', newItem);
+
+input.addEventListener('keypress', function (event) {
+    let key = event.keyCode;
     
-    let id = getId(target);
-    deleteFromArray(id);
-    target.parentNode.remove();
-}
-
-function deleteFromArray (id){
-
-    for (var i = 0; i <= array.length-1; i++) {
-        if (array[i].id == id) {
-            array.splice(i,1);
-        }
+    if (key === 13) {
+        newItem();
     }
-}
+});
+   
+ul.addEventListener('click', function (event) { // Smisliti bolje resenje
+    let target = event.target;
 
-function getId(target){
-    let li = target.parentNode;
-    let checkbox = li.firstChild;
-    let id = checkbox.id;
+    if (target.classList.contains('checkbox')) {
+        toggleDone(target);
+        return;
+    }
 
-    return id;
-}
-
-function saveToStorage(item) {
-
-    
-
-    
-    // var key = "" + item.id;
-    // var storageItem = JSON.stringify(item);
-    // localStorage.setItem(key, storageItem);
-
-    // for (var key in localStorage) {
-    //     console.log(key + ':' + localStorage[key]);
-    //   }
-
-
-
-    //localStorage.clear();
-}
-
+    if (target.classList.contains('delete-button')) {
+        deleteItem(target);
+        return;
+    }
+});
